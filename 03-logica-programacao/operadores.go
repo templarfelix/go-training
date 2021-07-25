0028b98553fe152b8fd22a0c35d7823677ddd9ba
Fernando Luís da Silva <felix.templar@gmail.com>
2021-07-25 18:32:05 -0300

    debug operators

---
+++ 03-logica-programacao/operadores.go
@@ -0,0 +1,65 @@
+package main
+
+import "fmt"
+
+// baseado em: https://www.digitalocean.com/community/tutorials/understanding-boolean-logic-in-go-pt
+func main() {
+	x := 5
+	y := 8
+
+	OpEquals(x, y)
+	OpNotEquals(x, y)
+	// adicione um exemplo de chamada de cada metodo
+
+}
+
+func OpEquals(x int, y int) bool {
+	fmt.Println("x == y:", x == y)
+	return x == y
+}
+
+func OpNotEquals(x int, y int) bool {
+	fmt.Println("x != y:", x != y)
+	return x != y
+}
+
+func OpLess(x int, y int) bool {
+	fmt.Println("x < y:", x < y)
+	return x < y
+}
+
+func OpGreater(x int, y int) bool {
+	fmt.Println("x > y:", x > y)
+	return x > y
+}
+
+func OpLessOrEqual(x int, y int) bool {
+	fmt.Println("x <= y:", x <= y)
+	return x <= y
+}
+
+func OpGreaterOrEqual(x int, y int) bool {
+	fmt.Println("x >= y:", x >= y)
+	return x >= y
+}
+
+func PossoFazerUmSaque(saldo int, saque int) bool {
+	// FIXME implementar logica
+
+	return false
+}
+
+/*
+
+Escreva nesta função os seguintes resultados para o calculo
+
+	se o saldo for suficiente para o saque = SALDO_SUFICIENTE
+	se o saldo for igual ao saque = SALDO_SUFICIENTE_IRA_ZERAR_O_SALDO
+	se o saldo for menor que o saque = SEM SALDO
+
+*/
+func StatusSaque(saldo int, saque int) string {
+	// FIXME implementar logica
+
+	return ""
+}
---
+++ 03-logica-programacao/operadores_test.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+"testing"
+)
+
+func TestOpEquals(t *testing.T) {
+	result := OpEquals(5, 5)
+	if result == false {
+		t.Errorf("OpEquals(5, 5) = %t; want true", result)
+	}
+}
+
+func TestFailOpEquals(t *testing.T) {
+	result := OpEquals(5, 4)
+	if result == true {
+		t.Errorf("OpEquals(5, 4) = %t; want false", result)
+	}
+}
+
+func TestOpNotEquals(t *testing.T) {
+	result := OpNotEquals(5, 5)
+	if result == true {
+		t.Errorf("OpNotEquals(5, 5) = %t; want false", result)
+	}
+}
+
+func TestFailOpNotEquals(t *testing.T) {
+	result := OpNotEquals(5, 4)
+	if result == false {
+		t.Errorf("OpNotEquals(5, 4) = %t; want true", result)
+	}
+}
+
+func TestOpThen(t *testing.T) {
+	result := OpLess(5, 4)
+	if result == true {
+		t.Errorf("OpLessThen(5, 4) = %t; want false", result)
+	}
+}
+
+func TestOpThen2(t *testing.T) {
+	result := OpLess(5, 5)
+	if result == true {
+		t.Errorf("OpLessThen(5, 4) = %t; want false", result)
+	}
+}
+
+func TestOpThen3(t *testing.T) {
+	result := OpLess(5, 6)
+	if result == false {
+		t.Errorf("OpLessThen(5, 4) = %t; want true", result)
+	}
+}
+
+// escreva os testes para OpGreater
+
+// escreva os testes para OpLessOrEqual
+
+// escreva os testes para OpGreaterOrEqual
