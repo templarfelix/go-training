cdcaf5bfaf94d70c6ce73dddbc02588f3c92ea22
Fernando Luís da Silva <felix.templar@gmail.com>
2021-08-11 21:17:59 -0300

    exercicio criar pessoa

---
+++ 05-struct/exercicio.go
@@ -0,0 +1,17 @@
+package main
+
+
+// FIXME criar o objeto Pessoa
+// irmaos
+// pais
+// pets
+
+// criar o metodo criaEU
+//
+func main() {
+
+	// mauricio -- irmao > tiago
+	// mauricio -- pet  > dudu
+	// mauricio -- pais > magda
+
+}
